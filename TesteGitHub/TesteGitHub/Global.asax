95de17028d40293e5ff121c01b2d314aafec402d
vrnery <vrnery@restinga.ifrs.edu.br>
2016-05-19 10:41:14 -0300

    Teste

---
+++ TesteGitHub/TesteGitHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TesteGitHub.MvcApplication" Language="C#" %>
---
+++ TesteGitHub/TesteGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TesteGitHub.Startup))]
+namespace TesteGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
