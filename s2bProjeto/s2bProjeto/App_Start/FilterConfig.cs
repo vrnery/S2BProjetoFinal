12e39122a1cead16e8d89c441c5e1dc7de14b291
Renan T Machado <renantabares@restinga.ifrs.edu.br>
2016-05-23 10:46:01 -0300

    Add projeto com templates para start

---
+++ s2bProjeto/s2bProjeto/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace s2bProjeto
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ s2bProjeto/s2bProjeto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(s2bProjeto.Startup))]
+namespace s2bProjeto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
