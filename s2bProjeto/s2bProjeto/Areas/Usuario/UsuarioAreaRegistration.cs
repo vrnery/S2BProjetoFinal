9d3f60159be310c240ff6636a364685b82ad4a8e
Renan Tabares <renantabares@restinga.ifrs.edu.br>
2016-06-02 09:45:01 -0300

    Areas usuário e admin

---
+++ s2bProjeto/s2bProjeto/Areas/Usuario/UsuarioAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace s2bProjeto.Areas.Usuario
+{
+    public class UsuarioAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Usuario";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Usuario_default",
+                "Usuario/{controller}/{action}/{id}",
+                new {controller="Home",  action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
